b0304760be80051b892bc5157b015959a816588d
Andrea Alicino <andrea.alicino@gmail.com>
2023-08-17 10:00:38 -0400

    Assignment 1: Explosive Barrel

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp; 
+
+	ForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("ForceComp")); 
+	ForceComp->SetupAttachment(MeshComp); 
+
+	ForceComp->SetAutoActivate(false);
+	ForceComp->Radius = 750.0f;
+	ForceComp->ImpulseStrength = 2500.0f;
+	ForceComp->bImpulseVelChange = true; //ignore mass of other objects 
+	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+}
+
+void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ForceComp->FireImpulse();
+}
+
