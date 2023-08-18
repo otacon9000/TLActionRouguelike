f518bafa07a1f32661cd738ffb7d1a12c735e6d3
Andrea Alicino <andrea.alicino@gmail.com>
2023-08-18 14:47:50 -0400

    PlayerHealth system
    - UI widget
    - event OnChanged
    - Aniamtion UI Text

--- Source/ActionRoguelike/Private/SAttributeComponent.cpp
+++ Source/ActionRoguelike/Private/SAttributeComponent.cpp
@@ -6,12 +6,6 @@
 // Sets default values for this component's properties
 USAttributeComponent::USAttributeComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
-
 	Health = 100.0f;
 }
 
@@ -19,6 +13,7 @@ bool USAttributeComponent::ApplyHealthChange(float Delta)
 {
 	Health += Delta;
 
+	OnHealthChanged.Broadcast(nullptr, this, Health, Delta);
 
 	return Health > 0 ? true : false;
 }
