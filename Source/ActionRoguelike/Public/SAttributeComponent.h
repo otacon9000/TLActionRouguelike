e62f53675b51d943a1060397a074a882d0920277
Andrea Alicino <andrea.alicino@gmail.com>
2023-08-31 17:42:49 -0400

    assignment 4

--- Source/ActionRoguelike/Public/SAttributeComponent.h
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -42,6 +42,9 @@ public:
 	bool IsFullHealth() const;
 
 	UFUNCTION(BlueprintCallable)
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable)
 	float GetHealthMax() const;
 
 	UPROPERTY(BlueprintAssignable)
